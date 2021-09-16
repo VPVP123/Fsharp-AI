3d210177db8e77ecb86519929ee95f16e7baee5a
Victor Svanqvist <svvi19ty@student.ju.se>
2021-09-16 19:48:50 +0200

    Added minmaxalpha + sent validMoves function to minimax F# function
    Co-Authored-By: Filip Larsson <55684571+fichlar@users.noreply.github.com>

--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -1,9 +1,7 @@
-﻿namespace FSAI
+﻿
+namespace FSAI
 
 module Minimax =
-
-    type Class1() = 
-        member this.X = "F#"
 
     let boardSize = int 8
     let empty = byte  0
@@ -11,6 +9,7 @@ module Minimax =
     let black = byte  2
     let valid = byte  3
     let tie =  byte 4
+
 
     let dirs =[
         (-1,1); (0,1); (1,1);
@@ -44,12 +43,13 @@ module Minimax =
             if corner = tile then myCorners <- [corner] |> List.append myCorners //Check if the corner in the loop is occupied by our tile and if it is add it to myCorners list
         myCorners.Length //Return  the length of the myCorners list to get all the inhabited corners of the tile type we are checking
 
-    let Evaluation (board : byte[,]) = //Evaluation function take a board
+
+    let Evaluation (validMoveFunc) (board : byte[,]) = //Evaluation function take a board
 
         let blackScore = GetScore board black //Get score for player black
         let whiteScore = GetScore board white //Get score for player white
-        let blackMobility = Reversi.Game.GetValidMoves board black //Get player blacks valid moves in the game //KALLA PÅ DENNA IFRÅN GAME:CS//
-        let whiteMobility = Reversi.Game.GetValidMoves board white //Get player blacks valid moves in the game //KALLA PÅ DENNA IFRÅN GAME:CS//
+        let blackMobility:(int * int)list = validMoveFunc board black //Get player blacks valid moves in the game //KALLA PÅ DENNA IFRÅN GAME:CS//
+        let whiteMobility:(int * int)list = validMoveFunc board white //Get player blacks valid moves in the game //KALLA PÅ DENNA IFRÅN GAME:CS//
 
         if blackScore = 0 then -200000 //if black score = 0 then return -200000
         elif whiteScore = 0 then 200000 //if whites score = 0 then return 200000
@@ -77,44 +77,58 @@ module Minimax =
             board.[fst flippedPiece, snd flippedPiece] <- tile //change the existing tile on the board the the new tile
         if not(flippedPieces.IsEmpty) then //If any flipped piece was missed
             board.[fst move, snd move] <- tile //Change the tile
+        board
         
 
-    let GetWinner (board : byte[,]) = 
+    let GetWinner (validMoveFunc) (board : byte[,]) = 
        let whiteScore = GetScore board white //Get score for white
        let blackScore = GetScore board black //Get score for black
        if whiteScore = 0 || blackScore = 0 || whiteScore + blackScore = 64 || 
-           List.length(Reversi.Game.GetValidMoves board black) + List.length(Reversi.Game.GetValidMoves board white) = 0 then // If statement to get stats for each player
+           List.length(validMoveFunc board black) + List.length(validMoveFunc board white) = 0 then // If statement to get stats for each player
            if blackScore > whiteScore then black //If blackScore greater then whiteScore then black return as winner
            elif whiteScore > blackScore then white //If whiteScore greater then blackScore then white return as winner
            else tie //If whiteScore is equal blackScore then return tie 
        else byte -1// Error? or empty
+
+
+    
    
-    let MinimaxAlphaBeta (board : byte[,]) (depth : int) (a : int) (b : int) (tile : byte) (isMaxPlayer : bool) =
-        if depth = 0 || GetWinner board <> empty then // Check if depth = 0 or if GetWinner is empty
-            Evaluation board //If so then return Evaluation board
+    let rec MinimaxAlphaBeta (validMoveFunc) (board : byte[,]) (depth : int) (a : int) (b : int) (tile : byte) (isMaxPlayer : bool) =
+        let rec RecMoveLoop (board : byte[,]) (validMoves : (int*int)list) (bestScore : int) (tile : byte) (isMaxPlayer : bool) (a : int) (b : int) =
+            match validMoves with
+                | [] -> bestScore
+                | head::tail ->
+                    let childBoard = MakeMove board head tile
+                    let nodeScore = MinimaxAlphaBeta validMoveFunc childBoard (depth - 1) a b (OtherTile tile) (not isMaxPlayer)
+                    if isMaxPlayer then //Check if its max player
+                        let newBestScore = max bestScore nodeScore //Max value of bestScore and nodeScore = bestScore
+                        let newA = max bestScore a// max of bestScore and a = a
+                        if b <= newA then
+                            newBestScore
+                        else
+                            RecMoveLoop board tail newBestScore tile isMaxPlayer newA b
+                    else
+                        let newBestScore = min bestScore nodeScore //Max value of bestScore and nodeScore = bestScore
+                        let newB = min bestScore b// max of bestScore and a = a
+                        if newB <= a then
+                            newBestScore
+                        else
+                            RecMoveLoop board tail newBestScore tile isMaxPlayer a newB
+
+
+        if depth = 0 || GetWinner validMoveFunc board <> empty then // Check if depth = 0 or if GetWinner is empty
+            Evaluation validMoveFunc board //If so then return Evaluation board
         else 
-            
             let bestScore = match isMaxPlayer with 
                             | true -> System.Int32.MinValue
                             | false -> System.Int32.MaxValue
+            let validMoves:(int * int)list= validMoveFunc board tile
 
-            let validMoves = Reversi.Game.GetValidMoves board tile
-            if validMoves.Length > 0 then //Check if validMoves is greater then 0
-                for move in validMoves do //For every move in validMoves then do
-                    let childBoard = MakeMove board move tile //Add childboard, move and tile to makeMove function
-                    let nodeScore = MinimaxAlphaBeta childBoard (depth - 1) a b  (otherTile tile) (not isMaxPlayer)
-                    if isMaxPlayer then //Check if its max player
-                        bestScore = max bestScore nodeScore //Max value of bestScore and nodeScore = bestScore
-                        a = max bestScore a// max of bestScore and a = a
-                    else 
-                        bestScore = min bestScore nodeScore //Min value of bestScore and nodeScore = bestScore
-                        b = min bestScore b // min value of bestScore and b = b
-                    //if b <= a then byte[] -1 //Add break here ? //If a is equal or greater then b, then return error/break
+            if validMoves.IsEmpty then
+                MinimaxAlphaBeta validMoveFunc board depth a b (OtherTile tile) (not isMaxPlayer)
+            else
+                RecMoveLoop board validMoves bestScore tile isMaxPlayer a b
 
-            else MinimaxAlphaBeta board depth a b (OtherTile tile) (not isMaxPlayer) //If validMoves is not greater then 0 then return minimaxalphabeta function
-            bestScore //Then return bestScore
-
-
-
+                   
 
 
--- Reversi/Game.cs
+++ Reversi/Game.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.FSharp.Core;
+using Microsoft.FSharp.Collections;
 
 namespace Reversi
 {
@@ -251,6 +252,13 @@ namespace Reversi
             return validMoves;
         }
 
+        public static int runMiniMax(byte[,] board, int depth, int a, int b, byte tile, bool isMaxPlayer)
+        {
+            var wrappedGetValidMoveFunc = FuncConvert.ToFSharpFunc<Tuple<byte[,], byte>, Microsoft.FSharp.Collections.FSharpList<Tuple<int, int>>>(t => ListModule.OfSeq(GetValidMoves(t.Item1, t.Item2)));
+            var AddGetValidMoveFunc = FuncConvert.FuncFromTupled(wrappedGetValidMoveFunc); // Skapa en FSharpFunc från den tuplade representationen
+            return FSAI.Minimax.MinimaxAlphaBeta(AddGetValidMoveFunc, board, depth, a, b, tile, isMaxPlayer); // Anropa F#-koden, skicka med vår wrappade funktion
+        }
+
         public static void MakeMove(byte[,] board, Tuple<int, int> move, byte tile)
         {
             List<Tuple<int, int>> flippedPieces = GetFlippedPieces(board, move, tile);
@@ -274,89 +282,89 @@ namespace Reversi
             return corners;
         }
 
-        public static int Evaluation(byte[,] board)
-        {
-            // Heuristical evaluation function that quantifies the attractiveness of a board,
-            // relative to the black player.
-            int evaluation = 0;
-            int blackScore = GetScore(board, Black);
-            int whiteScore = GetScore(board, White);
-            int blackMobility = GetValidMoves(board, Black).Count;
-            int whiteMobility = GetValidMoves(board, White).Count;
-            if (blackScore == 0)
-            {
-                return -200000;
-            }
-            else if (whiteScore == 0)
-            {
-                return 200000;
-            }
-            if (blackScore + whiteScore == 64 || blackMobility + whiteMobility == 0)
-            {
-                if (Black < whiteScore)
-                {
-                    return -100000 - whiteScore + blackScore;
-                }
-                else if (blackScore > whiteScore)
-                {
-                    return 100000 + blackScore - whiteScore;
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-            evaluation += blackScore - whiteScore;
-            if (blackScore + whiteScore > 55)
-            {
-                return (blackScore - whiteScore);
-            }
-            evaluation += (blackMobility - whiteMobility) * 10;
-            evaluation += (CountCorners(board, Black) - CountCorners(board, White)) * 100;
-            return evaluation;
-        }
+        //public static int Evaluation(byte[,] board)
+        //{
+        //    // Heuristical evaluation function that quantifies the attractiveness of a board,
+        //    // relative to the black player.
+        //    int evaluation = 0;
+        //    int blackScore = GetScore(board, Black);
+        //    int whiteScore = GetScore(board, White);
+        //    int blackMobility = GetValidMoves(board, Black).Count;
+        //    int whiteMobility = GetValidMoves(board, White).Count;
+        //    if (blackScore == 0)
+        //    {
+        //        return -200000;
+        //    }
+        //    else if (whiteScore == 0)
+        //    {
+        //        return 200000;
+        //    }
+        //    if (blackScore + whiteScore == 64 || blackMobility + whiteMobility == 0)
+        //    {
+        //        if (Black < whiteScore)
+        //        {
+        //            return -100000 - whiteScore + blackScore;
+        //        }
+        //        else if (blackScore > whiteScore)
+        //        {
+        //            return 100000 + blackScore - whiteScore;
+        //        }
+        //        else
+        //        {
+        //            return 0;
+        //        }
+        //    }
+        //    evaluation += blackScore - whiteScore;
+        //    if (blackScore + whiteScore > 55)
+        //    {
+        //        return (blackScore - whiteScore);
+        //    }
+        //    evaluation += (blackMobility - whiteMobility) * 10;
+        //    evaluation += (CountCorners(board, Black) - CountCorners(board, White)) * 100;
+        //    return evaluation;
+        //}
 
-        public static int MinimaxAlphaBeta(byte[,] board, int depth, int a, int b, byte tile, bool isMaxPlayer)
-        {
-            // The heart of our AI. Minimax algorithm with alpha-beta pruning to speed up computation.
-            // Higher search depths = greater difficulty.
-            if (depth == 0 || GetWinner(board) != Empty)
-            {
-                return Evaluation(board);
-            }
-            int bestScore;
-            if (isMaxPlayer) bestScore = int.MinValue;
-            else bestScore = int.MaxValue;
-            List<Tuple<int, int>> validMoves = GetValidMoves(board, tile);
-            if (validMoves.Count > 0)
-            {
-                foreach (Tuple<int, int> move in validMoves)
-                {
-                    byte[,] childBoard = board.Clone() as byte[,];
-                    MakeMove(childBoard, move, tile);
-                    int nodeScore = MinimaxAlphaBeta(childBoard, depth - 1, a, b, OtherTile(tile), !isMaxPlayer);
-                    if (isMaxPlayer)
-                    {
-                        bestScore = Math.Max(bestScore, nodeScore);
-                        a = Math.Max(bestScore, a);
-                    }
-                    else
-                    {
-                        bestScore = Math.Min(bestScore, nodeScore);
-                        b = Math.Min(bestScore, b);
-                    }
-                    if (b <= a)
-                    {
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                return MinimaxAlphaBeta(board, depth, a, b, OtherTile(tile), !isMaxPlayer);
-            }
-            return bestScore;
-        }
+        //public static int MinimaxAlphaBeta(byte[,] board, int depth, int a, int b, byte tile, bool isMaxPlayer)
+        //{
+        //    // The heart of our AI. Minimax algorithm with alpha-beta pruning to speed up computation.
+        //    // Higher search depths = greater difficulty.
+        //    if (depth == 0 || GetWinner(board) != Empty)
+        //    {
+        //        return Evaluation(board);
+        //    }
+        //    int bestScore;
+        //    if (isMaxPlayer) bestScore = int.MinValue;
+        //    else bestScore = int.MaxValue;
+        //    List<Tuple<int, int>> validMoves = GetValidMoves(board, tile);
+        //    if (validMoves.Count > 0)
+        //    {
+        //        foreach (Tuple<int, int> move in validMoves)
+        //        {
+        //            byte[,] childBoard = board.Clone() as byte[,];
+        //            MakeMove(childBoard, move, tile);
+        //            int nodeScore = MinimaxAlphaBeta(childBoard, depth - 1, a, b, OtherTile(tile), !isMaxPlayer);
+        //            if (isMaxPlayer)
+        //            {
+        //                bestScore = Math.Max(bestScore, nodeScore);
+        //                a = Math.Max(bestScore, a);
+        //            }
+        //            else
+        //            {
+        //                bestScore = Math.Min(bestScore, nodeScore);
+        //                b = Math.Min(bestScore, b);
+        //            }
+        //            if (b <= a)
+        //            {
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        return MinimaxAlphaBeta(board, depth, a, b, OtherTile(tile), !isMaxPlayer);
+        //    }
+        //    return bestScore;
+        //}
 
         public static Tuple<int, int> GetAIMove(byte[,] board, int depth, byte tile)
         {
@@ -390,7 +398,7 @@ namespace Reversi
                     int nodeScore;
                     if (tile == Black)
                     {
-                        nodeScore = MinimaxAlphaBeta(childBoard, depth - 1, int.MinValue, int.MaxValue, OtherTile(tile), false);
+                        nodeScore = runMiniMax(childBoard, depth - 1, int.MinValue, int.MaxValue, OtherTile(tile), false);
                         if (nodeScore > bestScore)
                         {
                             bestScore = nodeScore;
@@ -399,7 +407,7 @@ namespace Reversi
                     }
                     else
                     {
-                        nodeScore = MinimaxAlphaBeta(childBoard, depth - 1, int.MinValue, int.MaxValue, OtherTile(tile), true);
+                        nodeScore = runMiniMax(childBoard, depth - 1, int.MinValue, int.MaxValue, OtherTile(tile), true);
                         if (nodeScore < bestScore)
                         {
                             bestScore = nodeScore;
