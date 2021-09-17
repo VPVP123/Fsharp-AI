28e387e7147d5db3ad44d9d196274e6559e1f0e5
Filip Larsson <flarsson1999@gmail.com>
2021-09-17 10:15:55 +0200

    Added comments for functions

--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -11,7 +11,7 @@ module Minimax =
     let tie =  byte 4
 
 
-    let dirs =[
+    let dirs =[ //All possible directions for player tile
         (-1,1); (0,1); (1,1);
         (-1,1);         (1,0);
         (-1,-1); (0,-1);(1,-1);
@@ -26,16 +26,18 @@ module Minimax =
         let randomList = [(1, 2); (2, 3); (4, 4)] //placeholder
         randomList //placeholder
 
+    //Function OtherTile check tail color and return opposit
     let OtherTile tile = 
         if tile = black then white //check if player black then return player white
         elif tile = white then black //Opposit, check if player white then return player black
         else byte -1//error if not if npt value 1 or 2
 
+    //Function GetScore get score for tile
     let GetScore (board : byte[,]) (tile : byte) =
         Seq.cast<byte> board  //Set start value
         |> Seq.filter (fun cell -> cell = tile)  |> Seq.length //Filter each cell in the board
                    
-
+    //Function CountCorners count corner of the board of 8x8
     let CountCorners (board : byte[,]) (tile : byte) =
         let validCorners = [(board.[0,0]);(board.[0,7]);(board.[7,0]);(board.[7,7])] //Create a list of valid corners
         let mutable myCorners = [] //Create an empty list where we are going to append all the corners we are holding
@@ -44,6 +46,7 @@ module Minimax =
         myCorners.Length //Return  the length of the myCorners list to get all the inhabited corners of the tile type we are checking
 
 
+    // Evaluation function
     let Evaluation (validMoveFunc) (board : byte[,]) = //Evaluation function take a board
 
         let blackScore = GetScore board black //Get score for player black
@@ -88,46 +91,46 @@ module Minimax =
            if blackScore > whiteScore then black //If blackScore greater then whiteScore then black return as winner
            elif whiteScore > blackScore then white //If whiteScore greater then blackScore then white return as winner
            else tie //If whiteScore is equal blackScore then return tie 
-       else byte -1// Error? or empty
+       else byte -1 // Return empty
 
 
     
-   
+   //MinimaxAlphaBeta recursive function
     let rec MinimaxAlphaBeta (validMoveFunc) (board : byte[,]) (depth : int) (a : int) (b : int) (tile : byte) (isMaxPlayer : bool) =
         let rec RecMoveLoop (board : byte[,]) (validMoves : (int*int)list) (bestScore : int) (tile : byte) (isMaxPlayer : bool) (a : int) (b : int) =
-            match validMoves with
-                | [] -> bestScore
-                | head::tail ->
-                    let childBoard = MakeMove board head tile
-                    let nodeScore = MinimaxAlphaBeta validMoveFunc childBoard (depth - 1) a b (OtherTile tile) (not isMaxPlayer)
+            match validMoves with //Matches validMoves
+                | [] -> bestScore //If empty then return bestScore
+                | head::tail -> //Check from start to end
+                    let childBoard = MakeMove board head tile //Add childBaord which calls for MakeMNove function
+                    let nodeScore = MinimaxAlphaBeta validMoveFunc childBoard (depth - 1) a b (OtherTile tile) (not isMaxPlayer) //nodeScore is MinimaxAlphaBeta
                     if isMaxPlayer then //Check if its max player
-                        let newBestScore = max bestScore nodeScore //Max value of bestScore and nodeScore = bestScore
-                        let newA = max bestScore a// max of bestScore and a = a
-                        if b <= newA then
-                            newBestScore
-                        else
-                            RecMoveLoop board tail newBestScore tile isMaxPlayer newA b
+                        let newBestScore = max bestScore nodeScore //Max value of bestScore and nodeScore added to newBestScore
+                        let newA = max bestScore a// max of bestScore and a added to newA
+                        if b <= newA then //Check if newA is equal or greater then b
+                            newBestScore // if so then return newBestScore
+                        else //Else if b is greater or equal then newA
+                            RecMoveLoop board tail newBestScore tile isMaxPlayer newA b //Then return RecMoveLoop
                     else
-                        let newBestScore = min bestScore nodeScore //Max value of bestScore and nodeScore = bestScore
-                        let newB = min bestScore b// max of bestScore and a = a
-                        if newB <= a then
-                            newBestScore
-                        else
-                            RecMoveLoop board tail newBestScore tile isMaxPlayer a newB
+                        let newBestScore = min bestScore nodeScore //Max value of bestScore and nodeScore is added to newBestScore
+                        let newB = min bestScore b // max of bestScore and a is added to newB
+                        if newB <= a then //Check if a is equal or greater then newB
+                            newBestScore // if so then return newBestScore
+                        else //Else if newB is greater or equal then a
+                            RecMoveLoop board tail newBestScore tile isMaxPlayer a newB //Then return RecMoveLoop
 
 
         if depth = 0 || GetWinner validMoveFunc board <> empty then // Check if depth = 0 or if GetWinner is empty
             Evaluation validMoveFunc board //If so then return Evaluation board
-        else 
-            let bestScore = match isMaxPlayer with 
-                            | true -> System.Int32.MinValue
-                            | false -> System.Int32.MaxValue
-            let validMoves:(int * int)list= validMoveFunc board tile
+        else //Else if depth != 0 or GetWinner != empty
+            let bestScore = match isMaxPlayer with // Set bestScore and check with match if isMaxPlayer is true or false
+                            | true -> System.Int32.MinValue //If true then MinValue
+                            | false -> System.Int32.MaxValue //If false then MaxValue
+            let validMoves:(int * int)list= validMoveFunc board tile // Get function validMoves from game.cs (C#)
 
-            if validMoves.IsEmpty then
-                MinimaxAlphaBeta validMoveFunc board depth a b (OtherTile tile) (not isMaxPlayer)
-            else
-                RecMoveLoop board validMoves bestScore tile isMaxPlayer a b
+            if validMoves.IsEmpty then //Check if validMoves = empty 
+                MinimaxAlphaBeta validMoveFunc board depth a b (OtherTile tile) (not isMaxPlayer) //If empty then MinimaxAlphaBeta
+            else //Else if validMoves != empty 
+                RecMoveLoop board validMoves bestScore tile isMaxPlayer a b // If not empty then RecMoveLoop
 
                    
 
